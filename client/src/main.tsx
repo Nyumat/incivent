e710f0d0e142c84cabcfe280fc7504368fe3147c
Thomas <nyumat18@gmail.com>
2024-11-23 07:57:06 -0800

    more hopeful build fixes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,19 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
-import { Application } from "./app.tsx";
+import { BrowserRouter, Route, Routes } from "react-router";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import "./index.css";
+import { LandingPage } from "./landing";
+import Platform from "./platform";
+
+// eslint-disable-next-line react-refresh/only-export-components
+function Application() {
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/platform" element={<Platform />} />
+    </Routes>
+  );
+}
 
 export function renderToDom(container: HTMLElement) {
   createRoot(container).render(
