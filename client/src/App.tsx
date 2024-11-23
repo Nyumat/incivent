0e094d5fa8d7b7792065c8b9dd382322164a1b4b
Thomas <nyumat18@gmail.com>
2024-11-23 05:52:09 -0800

    add routing w/ rr

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,89 +1,12 @@
-import { useMemo, useState } from "react";
-import { createRoot } from "react-dom/client";
-import Map, {
-  FullscreenControl,
-  GeolocateControl,
-  Marker,
-  NavigationControl,
-  Popup,
-  ScaleControl,
-} from "react-map-gl";
-
-import CITIES from "./cities.json";
-import ControlPanel from "./Panel";
-import Pin from "./Pin";
-
-const TOKEN = import.meta.env.VITE_MAPBOX_TOKEN;
+import { Route, Routes } from "react-router";
+import { LandingPage } from "./landing";
+import Platform from "./platform";
 
 export default function App() {
-  const [popupInfo, setPopupInfo] = useState(null);
-
-  const pins = useMemo(
-    () =>
-      CITIES.map((city, index) => (
-        <Marker
-          key={`marker-${index}`}
-          longitude={city.longitude}
-          latitude={city.latitude}
-          anchor="bottom"
-          onClick={(e) => {
-            // If we let the click event propagates to the map, it will immediately close the popup with `closeOnClick: true`
-            e.originalEvent.stopPropagation();
-            setPopupInfo(city);
-          }}
-        >
-          <Pin />
-        </Marker>
-      )),
-    []
-  );
-
   return (
-    <>
-      <Map
-        initialViewState={{
-          latitude: 40,
-          longitude: -100,
-          zoom: 3.5,
-          bearing: 0,
-          pitch: 0,
-        }}
-        mapStyle="mapbox://styles/mapbox/dark-v9"
-        mapboxAccessToken={TOKEN}
-      >
-        <GeolocateControl position="top-left" />
-        <FullscreenControl position="top-left" />
-        <NavigationControl position="top-left" />
-        <ScaleControl />
-
-        {pins}
-
-        {popupInfo && (
-          <Popup
-            anchor="top"
-            longitude={Number(popupInfo.longitude)}
-            latitude={Number(popupInfo.latitude)}
-            onClose={() => setPopupInfo(null)}
-          >
-            <div>
-              {popupInfo.city}, {popupInfo.state} |{" "}
-              <a
-                target="_new"
-                href={`http://en.wikipedia.org/w/index.php?title=Special:Search&search=${popupInfo.city}, ${popupInfo.state}`}
-              >
-                Wikipedia
-              </a>
-            </div>
-            <img width="100%" src={popupInfo.image} />
-          </Popup>
-        )}
-      </Map>
-
-      <ControlPanel />
-    </>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/platform" element={<Platform />} />
+    </Routes>
   );
-}
-
-export function renderToDom(container) {
-  createRoot(container).render(<App />);
 }
