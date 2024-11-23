c177663ef08e22585311c602c21d483eb31bca9b
Thomas <nyumat18@gmail.com>
2024-11-23 09:59:35 -0800

    tanstack query setup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { ThemeProvider } from "./components/theme-provider.tsx";
 import "./index.css";
 import { LandingPage } from "./landing";
 import Platform from "./platform";
+
+const queryClient = new QueryClient();
 
 // eslint-disable-next-line react-refresh/only-export-components
 function Application() {
@@ -18,9 +22,12 @@ function Application() {
 export function renderToDom(container: HTMLElement) {
   createRoot(container).render(
     <BrowserRouter>
-      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-        <Application />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+          <Application />
+        </ThemeProvider>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </BrowserRouter>
   );
 }
