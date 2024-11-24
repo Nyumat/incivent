9d25eda3fcc7e0fcb556eb1a726988b4e18ee9f5
Thomas <nyumat18@gmail.com>
2024-11-23 19:55:00 -0800

    root route and health check

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import authRoutes from "@/routes/auth";
+import incidentRoutes from "@/routes/incident";
 import dotenv from "dotenv";
 import express from "express";
 
@@ -7,6 +8,15 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+app.get("/api/health", (req, res) => {
+  res.send("OK");
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the API for Incivent!");
+});
+
 app.use("/api/auth", authRoutes);
+app.use("/api", incidentRoutes);
 
 export default app;
